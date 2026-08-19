86b20cf37b074d23b4db01ded20442f7e6ea7f74
Morteza Ayashi <mortezaayashi.developer@gmail.com>
2026-08-19 17:45:57 +0330

    fix: update Vite config for module assets and favicon

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { glob } from 'glob';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'Modules/Home/resources/js/app.js',
-                'Modules/Home/resources/css/app.css',
+                ...glob.sync('Modules/*/resources/js/main.js'),
+                ...glob.sync('Modules/*/resources/js/app.js'),
+
+                ...glob.sync('Modules/*/resources/css/app.css'),
+                ...glob.sync('Modules/*/resources/css/main.css'),
+
+                ...glob.sync('resources/images/**/*.{jpg,jpeg,png,gif,svg,webp,ico}'),
+                ...glob.sync('resources/images/favicon/**/*.{ico,png}'),
+
+                'node_modules/@fortawesome/fontawesome-free/css/all.min.css',
             ],
             refresh: true,
         }),
